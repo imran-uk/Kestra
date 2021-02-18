8400d0f2acb54d4491125f9ebcb78731a59675c9
Imran Chaudhry <imran.chaudhry@stepstone.co.uk>
2021-02-18 11:12:29 +0000

    Add Vehicle model and some example vehicles

--- FleetManager.API/Controllers/VehicleController.cs
+++ FleetManager.API/Controllers/VehicleController.cs
@@ -3,10 +3,13 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using FleetManager.API.Models;
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
 // created via Add -> Item -> controller, -> webapi controller with read/write actions
+
+// TODO can I replace return types with Action/Task?
 
 namespace FleetManager.API.Controllers
 {
@@ -14,12 +17,43 @@ namespace FleetManager.API.Controllers
     [ApiController]
     public class VehicleController : ControllerBase
     {
+        // lets create a manual database of vehicles as a private var
+        // useful to do this when you have no actual database
+        private List<VehicleModel> _vehiclesDatabase = new List<VehicleModel>
+        {
+            new VehicleModel
+            {
+                Model = "Firebird", 
+                Make = "Pontiac", 
+                ProductionYear = 1986,
+                FriendlyName = "KITT"
+            },
+            new VehicleModel
+            {
+                Model = "DMC-12", 
+                Make = "DeLorean", 
+                ProductionYear = 1984,
+                FriendlyName = "Time Machine"
+            },
+            new VehicleModel
+            {
+                Model = "Civic", 
+                Make = "Honda", 
+                ProductionYear = 2007,
+                FriendlyName = "Rhonda"
+            }
+        };
+
+
         // GET: api/<VehicleController>
         [HttpGet]
-        public IEnumerable<string> Get()
+        public IEnumerable<VehicleModel> Get()
         {
-            return new string[] { "value1", "value2" };
+            //return new string[] { "value1", "value2" };
+            return _vehiclesDatabase;
         }
+
+
 
         // GET api/<VehicleController>/5
         [HttpGet("{id}")]
---
+++ FleetManager.API/Models/VehicleModel.cs
@@ -0,0 +1,11 @@
+﻿namespace FleetManager.API.Models
+{
+    // can get inmspioration from WeatherForecast type
+    public class VehicleModel
+    {
+        public string Model { get; set; }
+        public string Make { get; set; }
+        public int ProductionYear { get; set; }
+        public string FriendlyName { get; set; }
+    }
+}
